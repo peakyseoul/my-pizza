744a76f046ea57bc18699f1d583e5a41d7ba8edc
peakyseoul <81403511+peakyseoul@users.noreply.github.com>
2022-02-14 16:08:25 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `My-Pizza.uz`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#fff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
