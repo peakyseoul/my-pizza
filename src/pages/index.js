af8c7cca632961eefa8a9b04ec44705200831593
peakyseoul <81403511+peakyseoul@users.noreply.github.com>
2022-02-14 15:22:53 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>My-pizza sahifasiga hush kelibsiz</h1>
+    <p>Mazali va sifatli pizzalar faqat bizda.</p>
+    <p>Biz bilan boglaning. Tel: +998990 040 8008</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
